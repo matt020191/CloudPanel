80cd7e4c6153ea1b00200f7a0c7ea9cad09a4e81
KnowMoreIT <jdixon@knowmoreit.com>
2014-11-17 07:48:10 -0600

    Updates to Exchange bulk editing

--- CloudPanel/Modules/CompanyModules/Exchange/BulkModule.cs
+++ CloudPanel/Modules/CompanyModules/Exchange/BulkModule.cs
@@ -13,14 +13,83 @@ namespace CloudPanel.Modules.CompanyModules.Exchange
     {
         private static readonly ILog logger = log4net.LogManager.GetLogger(typeof(BulkModule));
 
+        private enum ActionToTake
+        {
+            Enable = 0,
+            Change = 2,
+            Disable = 1
+        }
+
+        public enum EmailFormat
+        {
+            Username = 0,
+            Firstname = 1,
+            Lastname = 2,
+            FirstNameDotLastname = 3,
+            FirstnameLastname =4,
+            LastnameDotFirstname = 5,
+            LastnameFirstname = 6,
+            FirstInitialLastname = 7,
+            LastnameFirstInitial = 8
+        }
+
         public BulkModule() : base("/company/{CompanyCode}/exchange/bulk")
         {
-            //this.RequiresAuthentication();
+            this.RequiresAuthentication();
 
             Get["/", c => c.Request.Accept("text/html")] = _ =>
             {
                 return View["Company/Exchange/Bulk.cshtml"];
             };
+
+            Post["/"] = _ =>
+            {
+                try
+                {
+                    string companyCode = _.CompanyCode;
+
+                    if (!Request.Form.CheckedUsers.HasValue)
+                        throw new Exception("No users were selected.");
+
+                    if (!Request.Form.ActionToTake.HasValue)
+                        throw new Exception("No action was selected.");
+
+                    logger.DebugFormat("Unparsed users is {0}", Request.Form.CheckedUsers.Value);
+                    string users = Request.Form.CheckedUsers.Value;
+                    string[] userPrincipalName = users.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    ActionToTake action = Enum.Parse(typeof(ActionToTake), Request.Form.ActionToTake.Value);
+                    switch (action)
+                    {
+                        case ActionToTake.Disable:
+                            logger.DebugFormat("Disable action was taken");
+                            break;
+                        default:
+                            throw new Exception("Unknown action was specified");
+                    }
+
+
+                    return Negotiate.WithModel(new { success = "" })
+                                    .WithView("error.cshtml");
+                }
+                catch (Exception ex)
+                {
+                    logger.ErrorFormat("Error bulk updating mailboxes: {0}", ex.ToString());
+
+                    ViewBag.error = ex.ToString();
+                    return Negotiate.WithModel(new { error = ex.Message })
+                                    .WithView("error.cshtml");
+                }
+                finally
+                {
+
+                }
+            };
+        }
+
+        private void DisableMailboxes(string[] userPrincipalNames)
+        {
+
         }
     }
 }
